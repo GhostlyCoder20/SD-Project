79982bc21fafe96ee0d2f59ca5db0b5cdf1b0fa6
Francisco Molina <GhostlyCoder20>
2023-07-03 17:18:32 -0600

    ¨featÃ: project created

---
+++ SysLab/src/main/java/com/ghostlycoder/syslab/SysLabApplication.java
@@ -0,0 +1,13 @@
+package com.ghostlycoder.syslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysLabApplication.class, args);
+	}
+
+}
---
+++ SysLab/src/test/java/com/ghostlycoder/syslab/SysLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghostlycoder.syslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
